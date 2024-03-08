6b586f8a75f63e9889aab36f904ec6ada511b9e9
Yifei9119 <71806425+Yifei9119@users.noreply.github.com>
2024-03-07 20:08:23 -0800

    create pet database

---
+++ app/src/main/java/com/example/iat359_petjournal/Constants.java
@@ -0,0 +1,12 @@
+package com.example.iat359_petjournal;
+
+public class Constants {
+    public static final String DATABASE_NAME = "petdatabase";
+    public static final String TABLE_NAME = "PETTABLE";
+    public static final String UID = "_id";
+    public static final String NAME = "Name";
+//    public static final String LOCATION = "Location";
+//    public static final String LATIN_NAME = "Latin_Name";
+    public static final String TYPE = "Type";
+    public static final int DATABASE_VERSION = 1;
+}
