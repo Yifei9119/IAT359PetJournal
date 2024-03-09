758a64aac16cb21f013ad02251fae4bb137d7c24
Yifei9119 <71806425+Yifei9119@users.noreply.github.com>
2024-03-08 21:42:31 -0800

    fix settings buttons

--- app/src/main/java/com/example/iat359_petjournal/Settings.java
+++ app/src/main/java/com/example/iat359_petjournal/Settings.java
@@ -4,28 +4,35 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Settings extends Activity {
-
-//    declare variables
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 //      initialize variables
+        Button logout = findViewById(R.id.logout);
+        ImageButton backButton = findViewById(R.id.backButton);
 
-
-    }
+//       when logout button is clicked go to login.class
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Login.class);
+                startActivity(intent);
+            }
+        });
 
 //    when back button is clicked go to home page
-    protected void gotoHomePage(View v){
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-    }
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
-//    when logout button is clicked
-    protected void gotoLogin(View v){
-        Intent intent = new Intent(this, Login.class);
-        startActivity(intent);
     }
 }
