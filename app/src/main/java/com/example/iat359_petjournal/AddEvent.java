59e05c0eba6c54d470771b49740b971576fe42e3
vfa1209 <vfa1209@gmail.com>
2024-04-04 02:24:54 -0700

    Schedule progress timepicker

--- app/src/main/java/com/example/iat359_petjournal/AddEvent.java
+++ app/src/main/java/com/example/iat359_petjournal/AddEvent.java
@@ -1,5 +1,8 @@
 package com.example.iat359_petjournal;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.nfc.FormatException;
 import android.os.Bundle;
@@ -8,29 +11,57 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AddEvent extends AppCompatActivity implements TextWatcher {
     private EditText taskEdit, startTimeEdit, endTimeEdit;
     private int startTime, endTime;
     private String taskName;
     MyDatabase db;
-    private Button addEventButton;
+    private Button addEventButton, cancelButton;
+
+    private DatePickerDialog datePickerDialog;
+    private Button selectDateButton, startTimeButton, endTimeButton;
+
+    int hour, minute;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_event);
 
+        initDatePicker();
+
         taskEdit = findViewById(R.id.taskEditText);
         taskEdit.addTextChangedListener(this);
-        startTimeEdit = findViewById(R.id.startTimeEdit);
-        startTimeEdit.addTextChangedListener(this);
-        endTimeEdit = findViewById(R.id.endTimeEdit);
-        endTimeEdit.addTextChangedListener(this);
+        selectDateButton = findViewById(R.id.selectDateButton);
+        selectDateButton.setText(getTodaysDate());
+        selectDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+
+        startTimeButton = findViewById(R.id.startTimeButton);
+        startTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initTimePicker();
+            }
+        });
+
+
+
         addEventButton = findViewById(R.id.addButton);
         addEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,12 +77,88 @@ public class AddEvent extends AppCompatActivity implements TextWatcher {
                     Toast.makeText(v.getContext(), "Please enter a task and the start and end time", Toast.LENGTH_SHORT).show();
                 }
 
+            }
+        });
 
+        cancelButton = findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(v.getContext(), Schedule.class);
+                startActivity(intent);
             }
         });
         db = new MyDatabase(this);
 
     }
+
+    private String getTodaysDate(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        month = month + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+    private void initDatePicker(){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day){
+                month = month +1;
+                String date = makeDateString(day, month, year);
+                startTimeButton.setText(date);
+            }
+
+        };
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+
+    }
+
+    private void initTimePicker(){
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                hour = selectedHour;
+                minute = selectedMinute;
+                startTimeButton.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+            }
+        };
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute, true);
+        timePickerDialog.setTitle("selected Time");
+        timePickerDialog.show();
+    }
+
+    private String makeDateString(int day, int month, int year){
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month){
+        if (month == 1) return "JAN";
+        if (month == 2) return "FEB";
+        if (month == 3) return "MAR";
+        if (month == 4) return "APR";
+        if (month == 5) return "MAY";
+        if (month == 6) return "JUN";
+        if (month == 7) return "JULY";
+        if (month == 8) return "AUG";
+        if (month == 9) return "SEPT";
+        if (month == 10) return "OCT";
+        if (month == 11) return "NOV";
+        if (month == 12) return "DEC";
+
+        return "JAN";
+
+    }
+
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -62,24 +169,6 @@ public class AddEvent extends AppCompatActivity implements TextWatcher {
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         if (taskEdit.isFocused()){
             taskName = s.toString();
-        }
-
-        if (startTimeEdit.isFocused()){
-            try {
-                startTime = (int) Double.parseDouble(s.toString());
-            }
-            catch (NumberFormatException e){
-                startTime = 0;
-            }
-        }
-
-        if (endTimeEdit.isFocused()){
-            try {
-                endTime = (int) Double.parseDouble(s.toString());
-            }
-            catch (NumberFormatException e){
-                startTime = 0;
-            }
         }
 
     }
