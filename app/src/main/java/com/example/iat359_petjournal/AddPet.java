be7de3b4ce633262a24e9164ab6421a88ccc53f0
vfa1209 <vfa1209@gmail.com>
2024-04-09 01:16:08 -0700

    UI

--- app/src/main/java/com/example/iat359_petjournal/AddPet.java
+++ app/src/main/java/com/example/iat359_petjournal/AddPet.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
